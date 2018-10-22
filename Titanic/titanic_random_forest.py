e2e5d17f1a195737bffe5377905aa1e55bfb1224
Alec Kerrigan <imor231@gmail.com>
2018-10-22 19:56:35 -0400

    Minor edits

--- Titanic/titanic_random_forest.py
+++ Titanic/titanic_random_forest.py
@@ -4,10 +4,27 @@ from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.utils import shuffle
 
 def load_training_data(filename):
-	columns = pd.Index(["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "Survived"], name='cols')
+	columns = pd.Index(["Name","Title", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "Survived"], name='cols')
 
 	data = pd.DataFrame(pd.read_csv(filename), columns = columns)
-	data = shuffle(data)
+	print(data.size)
+	
+	# Create the title feature 
+	data["Title"] = data.Name.str.extract(' ([A-Za-z]+)\.')
+
+	data['Title'] = data['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
+ 	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+	data['Title'] = data['Title'].replace('Mlle', 'Miss')
+	data['Title'] = data['Title'].replace('Ms', 'Miss')
+	data['Title'] = data['Title'].replace('Mme', 'Mrs')
+
+	# Map the remaining title feature to values 
+	title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+	data['Title'] = data['Title'].map(title_mapping)
+	data['Title'] = data['Title'].fillna(0)
+
+	data = data.drop("Name", axis=1)
 
 	# Replace female with binary
 	data = data.replace("female", 1)
@@ -32,11 +49,18 @@ def load_training_data(filename):
 	# Also fill some stray embarked Na
 	data = data.fillna(value={"Age":(total/m), "Embarked": 0})
 
+	shuffle(data)
+
+	trainY = data["Survived"]
+	trainX = data.drop("Survived", axis=1)
+	return trainX, trainY
+
+	"""
+	Version where we cross validated the data
 	live = data[data["Survived"] == 1]
 	dead = data[data["Survived"] == 0]
 
-
-	split = 300
+	split = 200
 	trainY = [live["Survived"][:split], dead["Survived"][:split]]
 	trainY = pd.concat(trainY)
 	trainX = live.drop("Survived", axis=1)[:split], dead.drop("Survived", axis=1)[:split]
@@ -46,13 +70,29 @@ def load_training_data(filename):
 	valY = pd.concat(valY)
 	valX = live.drop("Survived", axis=1)[split:], dead.drop("Survived", axis=1)[split:]
 	valX = pd.concat(valX)
-
-	return trainX, trainY, valX, valY
+	"""
 
 def load_testing_data(filename):
-	columns = pd.Index(["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"], name='cols')
+	columns = pd.Index(["Name", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"], name='cols')
 
 	data = pd.DataFrame(pd.read_csv(filename), columns = columns)
+
+	# Create the title feature 
+	data["Title"] = data.Name.str.extract(' ([A-Za-z]+)\.')
+
+	data['Title'] = data['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
+ 	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+	data['Title'] = data['Title'].replace('Mlle', 'Miss')
+	data['Title'] = data['Title'].replace('Ms', 'Miss')
+	data['Title'] = data['Title'].replace('Mme', 'Mrs')
+
+	# Map the remaining title feature to values 
+	title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+	data['Title'] = data['Title'].map(title_mapping)
+	data['Title'] = data['Title'].fillna(0)
+
+	data = data.drop("Name", axis=1)
 	
 	# Replace female with binary
 	data = data.replace("female", 1)
@@ -63,7 +103,8 @@ def load_testing_data(filename):
 	data = data.replace("S", 1)
 	data = data.replace("C", 2)
 
-	data = data.fillna(value={"Age":(30), "Embarked": 0, "Fare": 30})
+	print(data)
+	data = data.fillna(value={"Age":(30), "Embarked": 0, "Fare": 10})
 
 	return data
 
@@ -79,21 +120,18 @@ def output_results(results):
 if __name__ == "__main__":
 	
 	
-	model = RandomForestClassifier(n_estimators=50, n_jobs=1)
+	model = RandomForestClassifier(n_estimators=1000, n_jobs=1)
 
-	trainX, trainY, valX, valY = load_training_data("data/train.csv")
+	trainX, trainY = load_training_data("data/train.csv")
 	
-	
+
 	model.fit(trainX, trainY)
-	model_score = model.score(valX, valY)
 	
 	testX = load_testing_data("data/test.csv")
 	
 	results = model.predict(testX)
-	print(model_score)
-	# I want at least 80% on the testing stage before outputting
-	if model_score >= 0.8:
-		output_results(results)
+	output_results(results)
+
 	
 	
 	
